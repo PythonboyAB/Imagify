9657395c4744d18ecd5dd5b023ba93488fdffb75
Abhishek Sahani <abh792002@gmail.com>
2026-02-22 16:31:45 +0530

    Home page compeleted

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Home from "./pages/Home";
+import BuyCredits from "./pages/BuyCredits";
+import Result from "./pages/Result";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <div className="px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-linear-to-b from-teal-50 to-orange-50 ">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/buy" element={<BuyCredits />} />
+        <Route path="/result" element={<Result />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/components/Testimonial.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { assets, testimonialsData } from "../assets/assets.js";
+
+const Testimonial = () => {
+  return (
+    <div className="flex flex-col items-center justify-center my-20 py-12">
+      <h1 className="text-3xl sm:text-4xl font-semibold">
+        Customer testimonials
+      </h1>
+      <p className="text-gray-500 mb-8">What Our Users Are Saying</p>
+      <div className="flex flex-wrap gap-6">
+        {testimonialsData.map((item, index) => (
+          <div
+            key={index}
+            className="bg-white/20 p-12 rounded-lg shadow-md order w-80 m-auto cursor-pointer hover:scale-[1.02] transition-all"
+          >
+            <div className="flex flex-col items-center">
+              <img src={item.image} alt="" className="rounded-full w-14" />
+              <h2 className="text-xl font-semibold mt-3">{item.name}</h2>
+              <p className="text-gray-500 mb-4">{item.role}</p>
+              <div className="flex mb-4">
+                {Array(item.stars)
+                  .fill()
+                  .map((item, index) => (
+                    <img src={assets.rating_star} alt="stars" key={index} />
+                  ))}
+              </div>
+              <p className=" text-center text-sm text-gray-600"> {item.text}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Testimonial;
